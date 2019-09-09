eec7d5e8f44327d3ce551d9710e5be5f64b173fe
YourSouLi5Mine <precichysim@gmail.com>
2019-09-08 23:50:06 -0500

    page 35

---
+++ networking/net-watcher-json-service.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const fs = require('fs');
+const net = require('net');
+const filename = process.argv[2];
+
+if (!filename) {
+  throw Error('Error: No filename specified');
+}
+
+net.createServer(connection => {
+  console.log('Subscriber connected.');
+  connection.write(JSON.stringify({type: 'watching', file: filename}) + '\n');
+
+  const watcher =
+    fs.watch(filename, () => connection.write(JSON.stringify({type: 'changed', timestamp: Date.now()}) + '\n'));
+
+  connection.on('close',  () => {
+    console.log('Sbuscribers disconnected.');
+    watcher.close();
+  });
+}).listen('/tmp/watcher.sock', () => console.log('Listening for subscribers...'));
+
